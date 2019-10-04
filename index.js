345fe78357710ed1f55a253ce89a9fadc16cd0c2
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-10-04 01:00:28 -0300

    Passando string para sem tls

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose')
-const fs = require('fs')
-const url = 'mongodb://root:rootroot@docdb-2019-10-04-00-27-36.cluster-cizjcxlcyxgi.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0'
-
+// const fs = require('fs')
+//const url = 'mongodb://root:rootroot@docdb-2019-10-04-01-37-24.cizjcxlcyxgi.us-east-1.docdb.amazonaws.com:27017/teste?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem'
+const url = 'mongodb://root:rootroot@docdb-2019-10-04-01-37-24.cluster-cizjcxlcyxgi.us-east-1.docdb.amazonaws.com:27017/teste'
+'mongodb://root:rootroot@docdb-2019-10-04-01-37-24.cluster-cizjcxlcyxgi.us-east-1.docdb.amazonaws.com:27017'
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  ssl: true,
-  sslValidate: false,
-  sslCA: fs.readFileSync('./rds-combined-ca-bundle.pem')
 })
   .then(() => console.log('Connection to DB successful'))
   .catch((err) => console.error('ERROR >>>>>>>> ', err));
